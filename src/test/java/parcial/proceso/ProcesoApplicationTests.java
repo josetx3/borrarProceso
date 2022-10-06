3a2e2683f83f2a8652fc1b07a59964089ce71fd4
josetx3 <joseleoquintero123@gmail.com>
2022-10-06 12:41:31 -0500

    prueba

---
+++ src/test/java/parcial/proceso/ProcesoApplicationTests.java
@@ -0,0 +1,13 @@
+package parcial.proceso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
